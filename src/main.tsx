8a356e3b4d5facb7390c3e83909adfbfecc3329a
Joel <jsabonete09@gmail.com>
2025-08-31 18:36:10 +0200

    Restore main App and update production environment variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import TestComponent from './TestComponent.tsx'
+import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<TestComponent />);
+createRoot(document.getElementById("root")!).render(<App />);
